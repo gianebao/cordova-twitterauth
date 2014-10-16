8d5891b2c7fa6e29744e25ca520ec30e93a08f4b
Peter Gerasimenko <gpstmp@gmail.com>
2014-10-17 00:15:51 +0400

    remove useless imports

--- src/ios/TwitterAuth.h
+++ src/ios/TwitterAuth.h
@@ -5,12 +5,7 @@
 //  Created by Petr G.
 //
 
-    #import <Foundation/Foundation.h>
-    #import <Twitter/Twitter.h>
-    #import <Accounts/Accounts.h>
-    #import <Cordova/CDVPlugin.h>
-    #import <Cordova/CDVJSON.h>
-
+#import <Cordova/CDV.h>
 
 @interface TwitterAuth : CDVPlugin
 
